3f6739d38c6eddb7d24f8428725ca481dc58da8f
trungngotdt <trungngotdt@outlook.com.vn>
2018-08-24 10:22:23 +0700

    Initial Project

---
+++ HelloWorldEther/HelloWorld.sol
@@ -0,0 +1,10 @@
+﻿pragma solidity ^0.4.7;
+contract HelloWorld
+{
+    string yourname;
+    function GetHello(string name) public returns (string)
+    {
+        yourname=name;
+        return yourname;
+    }
+}
